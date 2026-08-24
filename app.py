6fab6f66ff9386cda4878f5e33fb50cbda6c342a
ingdago <54037963+ingdago@users.noreply.github.com>
2026-08-23 20:35:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ st.markdown("""
 @st.cache_data
 def cargar_datos():
     # Lectura del archivo local en el repositorio
-    df = pd.read_csv('base_aac.csv', sep=None, engine='python', encoding='utf-8-sig')
+df = pd.read_excel('base_aac.xlsx', engine='openpyxl')
     
     # Normalización de la columna Costo Neto (Manejo de formato $ 1.292,00)
     if df['Costo neto'].dtype == 'O':
